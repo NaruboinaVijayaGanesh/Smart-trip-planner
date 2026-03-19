aebb02fe2835c5bb2ed0a3d4dd6660bb6db6382e
Kanneganti Sravika Chowdary <159680391+KannegantiSravikaChowdary@users.noreply.github.com>
2026-03-19 23:06:26 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,11 @@ from app import create_app
 
 app = create_app()
 
+# 👇 ADD THIS BLOCK
+with app.app_context():
+    db.create_all()
+
+
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "1") == "1"
     app.run(debug=debug_mode, use_reloader=False, threaded=False)
