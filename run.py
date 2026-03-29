9b3626b5db02eaf1a3d2b2124c17a2931c19e138
Kanneganti Sravika Chowdary <159680391+KannegantiSravikaChowdary@users.noreply.github.com>
2026-03-29 20:01:24 +0530

    Remove use_reloader and threaded options from app.run

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "1") == "1"
-    app.run(debug=debug_mode, use_reloader=False, threaded=False)
+    app.run(debug=debug_mode)
