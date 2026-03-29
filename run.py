dfcf3d942d44d0d23d8cc012517149c9c1e1d000
Kanneganti Sravika Chowdary <159680391+KannegantiSravikaChowdary@users.noreply.github.com>
2026-03-29 15:56:37 +0530

    Remove database creation from run.py
    Removed database creation block from app context.

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
 import os
 
 from app import create_app
-from app.extensions import db  
 
 app = create_app()
-
-# 👇 ADD THIS BLOCK
-with app.app_context():
-    db.create_all()
-
 
 if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "1") == "1"
